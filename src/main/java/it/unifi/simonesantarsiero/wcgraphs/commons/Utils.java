81130a19da6acf1a7af5306ec0139d31d910e067
simone.santarsiero <simone.santarsiero@jaewa.com>
2020-10-04 13:14:23 +0200

    Added properties file

--- src/main/java/it/unifi/simonesantarsiero/wcgraphs/commons/Utils.java
+++ src/main/java/it/unifi/simonesantarsiero/wcgraphs/commons/Utils.java
@@ -1,10 +1,19 @@
 package it.unifi.simonesantarsiero.wcgraphs.commons;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Properties;
+
 public class Utils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Utils.class);
+
     private Utils() {
     }
 
-    public static final String DATASETS_PATH = "/DATASETS/example/";
+    public static final String DATASETS_PATH = getProperty("datasetsPath");
 
     public static final String VALUE_GRAPH = "Graph";
     public static final String VALUE_DATASET = "dataset";
@@ -15,16 +24,29 @@ public class Utils {
 
     public static final String EXT_TSV = ".tsv";
     public static final String EXT_GRAPH = ".graph";
+    public static final String EXT_JAR = ".jar";
 
     public static final String SLASH = "/";
 
     public static final String RESET = "\033[0m";
     public static final String RED = "\033[0;31m";
 
-    private static final String APP_NAME = "wcgraphs-1.0-SNAPSHOT";
-    public static final String USAGE_ERROR_MESSAGE = colorize("\nUsage: java -cp " + APP_NAME + ".jar {} GRAPH_PATH\n\n");
+    private static final String APP_NAME = getProperty("artifactId");
+    private static final String APP_VERSION = getProperty("version");
+    private static final String JAR_NAME = APP_NAME + "-" + APP_VERSION + EXT_JAR;
+    public static final String USAGE_ERROR_MESSAGE = colorize("\nUsage: java -cp " + JAR_NAME + " {} GRAPH_PATH\n\n");
 
     private static String colorize(String message) {
         return RED + message + RESET;
+    }
+
+    private static String getProperty(String propertyName) {
+        Properties properties = new Properties();
+        try {
+            properties.load(Utils.class.getClassLoader().getResourceAsStream("wcgraphs.properties"));
+        } catch (IOException e) {
+            LOGGER.error("IOException: ", e);
+        }
+        return properties.getProperty(propertyName);
     }
 }
